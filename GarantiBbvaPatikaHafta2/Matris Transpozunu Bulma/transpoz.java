b7ecb1c19161edbf5d112e760fd56e782ca9d9c3
“Eray <90953626+Erayakg@users.noreply.github.com>
2023-07-15 15:39:41 +0300

    u

--- GarantiBbvaPatikaHafta2/Matris Transpozunu Bulma/transpoz.java
+++ GarantiBbvaPatikaHafta2/Matris Transpozunu Bulma/transpoz.java
@@ -9,23 +9,20 @@ import java.util.Scanner;
 
 public class Main {
     public static int[][] transpoze(int arr[][]) {
-        //satır ve sutün uzunlukları alınır
+   
 
         int rows = arr.length;
         int colums = arr[0].length;
 
-        //transpozunda tam tersi olacağı için sutün uzunluğu satır uzunluğu boyutunda tanımlanmışıtr
-        //aynı şekil satır uzunluğuda sutğn uznuluğunda tanımlanışıtr
-
         int newarr[][]=new int[colums][rows];
-        //verilen dizi gezilir
+
         for (int i=0;i<rows;i++){
             for (int j=0;j<colums;j++){
-                //satırdaki elemanlar yeni dizinin sutün elemanlarına yerleştirilir
+               
                newarr[j][i]=arr[i][j];
             }
         }
-        //ekrana yeni diziyi yazdırma
+      
         for (int i=0;i<colums;i++){
             for (int j=0;j<rows;j++){
                 System.out.print(newarr[i][j]);
